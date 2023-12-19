17d9502b437d0c73515ca7dfe24243dc08c5c90c
IvanTymoshchuk <timohin2231@gmail.com>
2023-12-19 11:17:40 +0000

    Deploying to gh-pages from @ IvanTymoshchuk/porten@c5c1847e84ed41916639fd136facef327bd64045 🚀

---
+++ index.fe609f5d.js
@@ -0,0 +1,2 @@
+$(".multiple-items").slick({dots:!0,infinite:!0,arrows:!1,speed:500,autoplaySpeed:2e3,slidesToShow:3,adaptiveHeight:!0,cssEase:"linear",autoplay:!0,responsive:[{breakpoint:600,settings:{slidesToShow:2}},{breakpoint:480,settings:{slidesToShow:1}}]});
+//# sourceMappingURL=index.fe609f5d.js.map
